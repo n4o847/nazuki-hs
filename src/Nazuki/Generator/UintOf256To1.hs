5ca8195867284bf3c9fde50f0f0ea6ae1d93cc54
n4o847 <yuu.miu.0925@gmail.com>
2019-12-15 00:57:04 +0900

    Add UintOf256To1 ( dup, not )

--- src/Nazuki/Generator/UintOf256To1.hs
+++ src/Nazuki/Generator/UintOf256To1.hs
@@ -3,6 +3,8 @@
 
 module Nazuki.Generator.UintOf256To1
     ( uintOf256To1Const
+    , uintOf256To1Dup
+    , uintOf256To1Not
     , uintOf256To1Add
     , uintOf256To1Sub
     , uintOf256To1Scan
@@ -26,10 +28,40 @@ uintOf256To1Const :: Int -> Oper
 uintOf256To1Const a = do
     let head = 0
     let body = 1
-
     consume 0
     add head 1
     add body (a `mod` 256)
+    produce 1
+
+uintOf256To1Dup :: Oper
+uintOf256To1Dup = do
+    let aHead = 0
+    let aBody = 1
+    let bHead = 2
+    let bBody = 3
+    consume 1
+    while aBody do
+        sub aBody 1
+        add bHead 1
+        add bBody 1
+    while bHead do
+        sub bHead 1
+        add aBody 1
+    add bHead 1
+    produce 2
+
+uintOf256To1Not :: Oper
+uintOf256To1Not = do
+    let head = 0
+    let body = 1
+    consume 1
+    while body do
+        set body 0
+        sub head 1
+    while head do
+        sub head 1
+        add body 1
+    add head 1
     produce 1
 
 uintOf256To1Add :: Oper
@@ -38,7 +70,6 @@ uintOf256To1Add = do
     let aBody = 1
     let bHead = 2
     let bBody = 3
-
     consume 2
     while bBody do
         sub bBody 1
@@ -52,7 +83,6 @@ uintOf256To1Sub = do
     let aBody = 1
     let bHead = 2
     let bBody = 3
-
     consume 2
     while bBody do
         sub bBody 1
@@ -66,7 +96,6 @@ uintOf256To1Scan = do
     let body = 1
     let input = 2
     let continue = 3
-
     consume 0
     add continue 1
     while continue do
