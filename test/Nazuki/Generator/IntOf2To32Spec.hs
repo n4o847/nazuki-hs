f25e21ca1cc09a7607b5aa3721c40aa01a64442d
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-01-09 00:07:30 +0900

    Add Spec

---
+++ test/Nazuki/Generator/IntOf2To32Spec.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Nazuki.Generator.IntOf2To32Spec (spec) where
+
+import Test.Hspec
+import Nazuki.Runner
+import Nazuki.Generator
+
+spec :: Spec
+spec = do
+    describe "intOf256To32Scan" $
+        it "intOf2To32Scan" $
+            let
+                program = generate do
+                    intOf2To32Scan
+                    intOf2To32Mul10
+                    intOf2To32Print
+                input = "13\n"
+                output = run program input
+                expected = Right "130"
+            in
+                output `shouldBe` expected
