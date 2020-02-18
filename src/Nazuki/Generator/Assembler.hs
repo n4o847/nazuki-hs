5aadd6a1bbfd4013f3c497bfc8fc577155bee919
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-02-19 00:14:38 +0900

    Add jump Issue: ジャンプ先に jump より小さい命令があると実行してしまう

--- src/Nazuki/Generator/Assembler.hs
+++ src/Nazuki/Generator/Assembler.hs
@@ -2,8 +2,9 @@
 {-# LANGUAGE LambdaCase #-}
 
 module Nazuki.Generator.Assembler
-  ( assemble
-  )
+    ( assemble
+    , jump
+    )
 where
 
 import           Control.Monad
@@ -12,58 +13,70 @@ import qualified Data.Map as Map
 import           Nazuki.Generator.Core
 import           Nazuki.Generator.Util
 
-assemble :: [(Int, Oper)] -> [Int] -> Oper
-assemble isa_ opcodes = do
-  let isa = Map.fromList isa_
-  let isize = Bits.finiteBitSize (Map.size isa - 1) - Bits.countLeadingZeros (Map.size isa - 1)
-  let tmp = 0
-  let cmd = (1 +)
-  bfDec
-  forward (isize + 1)
-  forM_ (reverse opcodes) \bits -> do
-    forM_ [0 .. isize - 1] \i -> do
-      when (Bits.testBit bits i) do
-        add (cmd i) 1
-    forward (isize + 1)
-  backward (isize + 1)
-  bfInc
-  while tmp do
-    sub tmp 1
-    let seeBit i bits = do
-          if i >= 0 then
-            ifElse (cmd i) tmp (
-              seeBit (i - 1) (Bits.setBit bits i)
-            ) (
-              seeBit (i - 1) bits
-            )
-          else do
-            forM_ (Map.lookup bits isa) \op -> do
-              ipToSp (isize + 1) 33
-              op
-              spToIp 33 (isize + 1)
-    seeBit (isize - 1) 0
-    add tmp 1
-    backward (isize + 1)
-    add tmp 1
+assemble :: Int -> Int -> [(Int, Oper)] -> [Int] -> Oper
+assemble isize ssize isa_ opcodes = do
+    let isa = Map.fromList isa_
+    let tmp = 0
+    let cmd = (1 +)
+    bfDec
+    forward isize
+    forM_ (reverse opcodes) \bits -> do
+        forM_ [0 .. isize - 2] \i -> do
+            when (Bits.testBit bits i) do
+                add (cmd i) 1
+        forward isize
+    backward isize
+    bfInc
+    while tmp do
+        sub tmp 1
+        let seeBit i bits = do
+                if i >= 0 then
+                    ifElse (cmd i) tmp (
+                        seeBit (i - 1) (Bits.setBit bits i)
+                    ) (
+                        seeBit (i - 1) bits
+                    )
+                else do
+                    forM_ (Map.lookup bits isa) \op -> do
+                        ipToSp isize ssize
+                        op
+                        spToIp isize ssize
+        seeBit (isize - 2) 0
+        add tmp 1
+        backward isize
+        add tmp 1
 
 spToIp :: Int -> Int -> Oper
-spToIp ssize isize = do
-  backward ssize
-  bfOpn
-  backward ssize
-  bfCls
-  backward isize
-  bfOpn
-  backward isize
-  bfCls
+spToIp isize ssize = do
+    backward ssize
+    bfOpn
+    backward ssize
+    bfCls
+    backward isize
+    bfOpn
+    backward isize
+    bfCls
 
 ipToSp :: Int -> Int -> Oper
 ipToSp isize ssize = do
-  forward isize
-  bfOpn
-  forward isize
-  bfCls
-  forward ssize
-  bfOpn
-  forward ssize
-  bfCls
+    forward isize
+    bfOpn
+    forward isize
+    bfCls
+    forward ssize
+    bfOpn
+    forward ssize
+    bfCls
+
+jump :: Int -> Int -> Int -> Oper
+jump isize ssize rel = do
+    spToIp isize ssize
+    if rel >= 0 then
+        replicateM_ rel do
+            bfInc
+            backward isize
+    else
+        replicateM_ (negate rel) do
+            forward isize
+            bfDec
+    ipToSp isize ssize
