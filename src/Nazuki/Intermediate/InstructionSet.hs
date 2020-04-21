0bf0ad9d34bf76602b870e6ee5d1f3eaa6b2f5c2
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-04-21 18:07:17 +0900

    Implement insToOper

--- src/Nazuki/Intermediate/InstructionSet.hs
+++ src/Nazuki/Intermediate/InstructionSet.hs
@@ -64,4 +64,36 @@ generate list = G.generate do
 insToOper :: Instruction -> G.Oper
 insToOper = \case
     Const x -> G.intOf2To32Const x
+    Dup -> G.intOf2To32Dup
+    Get x -> G.intOf2To32Get x
+    Set x -> G.intOf2To32Set x
+    Not -> G.intOf2To32Not
+    And -> G.intOf2To32And
+    Or -> G.intOf2To32Or
+    Xor -> G.intOf2To32Xor
+    Shl -> G.intOf2To32Shl
+    ShrU -> G.intOf2To32ShrU
+    ShrS -> G.intOf2To32ShrS
+    Inc -> G.intOf2To32Inc
+    Add -> G.intOf2To32Add
+    Sub -> G.intOf2To32Sub
+    Mul10 -> G.intOf2To32Mul10
+    Mul -> G.intOf2To32Mul
+    Eqz -> G.intOf2To32Eqz
+    Nez -> G.intOf2To32Nez
+    Eq -> G.intOf2To32Eq
+    LtS -> G.intOf2To32LtS
+    LeS -> G.intOf2To32LeS
+    LtU -> G.intOf2To32LtU
+    LeU -> G.intOf2To32LeU
+    GtS -> G.intOf2To32GtS
+    GeS -> G.intOf2To32GeS
+    GtU -> G.intOf2To32GtU
+    GeU -> G.intOf2To32GeU
+    Scan -> G.intOf2To32Scan
+    Print -> G.intOf2To32Print
+    Jump r -> G.intOf2To32Jump r
+    Jez r -> G.intOf2To32Jez r
+    Jnz r -> G.intOf2To32Jnz r
+    Jeq r -> G.intOf2To32Jeq r
     _ -> G.bfNop
