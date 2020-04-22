f05cb1af2a191dd145b79ee43a68adb3a6984464
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-04-22 18:02:15 +0900

    Add Intermediate.Complex

---
+++ src/Nazuki/Intermediate/Complex.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
+
+module Nazuki.Intermediate.Complex where
+
+import qualified Nazuki.Intermediate.InstructionSet
+                                               as I
+
+ifThen :: [I.Instruction] -> [I.Instruction] -> [I.Instruction]
+ifThen cond cons =
+    cond ++ I.Jez (length cons) : cons
+
+ifThenElse :: [I.Instruction] -> [I.Instruction] -> [I.Instruction] -> [I.Instruction]
+ifThenElse cond cons alt =
+    cond ++ I.Jez (length cons + 1) : cons ++ I.Jump (length alt) : alt
+
+whileDo :: [I.Instruction] -> [I.Instruction] -> [I.Instruction]
+whileDo cond cons =
+    cond ++ I.Jez (length cons + 1) : cons ++ [I.Jump (negate (length cond + 1 + length cons + 1))]
