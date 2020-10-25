f5c9233b6efe743af356e36b48fd110c3acb0861
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-10-26 05:03:45 +0900

    Update workflow

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p out
+
+ahc-link \
+  --input-hs wasm/Lib.hs \
+  --output-directory out \
+  --output-prefix nazuki \
+  --no-main \
+  --browser \
+  --ghc-option "-isrc" \
+  --export-function=generate
