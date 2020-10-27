30d7724a166efb3b61485de84d73f756e98c3451
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-10-27 20:57:14 +0900

    Output compile result

--- wasm/Lib.hs
+++ wasm/Lib.hs
@@ -3,10 +3,10 @@ module Lib where
 import Asterius.Types
 import qualified Nazuki.Intermediate.InstructionSet as I
 
-foreign export javascript "generate" generate :: Int -> Int
+foreign export javascript "generate" generate :: Int -> JSString
 
-generate :: Int -> Int
-generate x = length $ I.generate
+generate :: Int -> JSString
+generate x = toJSString $ I.generate
     [ I.Scan
     , I.Scan
     , I.Add
