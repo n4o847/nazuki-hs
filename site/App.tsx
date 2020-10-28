b90c0d8e3e833369e4f43fd2d4e00219295e1cea
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-10-28 22:49:40 +0900

    Add input and output form

--- site/App.tsx
+++ site/App.tsx
@@ -3,9 +3,14 @@ import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import Alert from 'react-bootstrap/Alert';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Nazuki } from './nazuki/types';
 
 export default function App () {
+  const [nazuki, setNazuki] = useState<Nazuki>();
   const [result, setResult] = useState('');
+  const [input, setInput] = useState('');
+  const [output, setOutput] = useState('');
   const [errors, setErrors] = useState<any[]>([]);
 
   const addError = (error: any) => {
@@ -16,11 +21,22 @@ export default function App () {
     (async () => {
       const { loadNazuki } = await import('./nazuki');
       const nazuki = await loadNazuki();
-      setResult(await nazuki.generate(10));
+      setNazuki(nazuki);
+      const result = await nazuki.generate(10);
+      setResult(result);
     })().catch((err) => {
       addError(`${err}`);
     });
   }, []);
+
+  const run = () => {
+    if (!nazuki) return;
+    nazuki.run(result, input).then((output) => {
+      setOutput(output);
+    }).catch((error) => {
+      addError(error);
+    });
+  };
 
   return (
     <>
@@ -34,14 +50,40 @@ export default function App () {
             <Alert key={idx} variant="danger">{error}</Alert>
           ))}
         </div>
-        <Form.Control
-          as="textarea"
-          readOnly
-          className="text-monospace"
-          style={{ wordBreak: 'break-all', height: '10rem' }}
-          value={result}
-        />
-        <p>{result.length}</p>
+        <Form>
+          <Form.Control
+            as="textarea"
+            readOnly
+            className="text-monospace"
+            style={{ wordBreak: 'break-all', height: '10rem' }}
+            value={result}
+          />
+          <p>{result.length}</p>
+          <Form.Group>
+            <Form.Label>Input</Form.Label>
+            <Form.Control
+              as="textarea"
+              className="text-monospace"
+              style={{ height: '10rem' }}
+              spellCheck={false}
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Button variant="primary" onClick={run}>Run</Button>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Output</Form.Label>
+            <Form.Control
+              as="textarea"
+              readOnly
+              className="text-monospace"
+              style={{ height: '10rem' }}
+              value={output}
+            />
+          </Form.Group>
+        </Form>
       </Container>
     </>
   );
