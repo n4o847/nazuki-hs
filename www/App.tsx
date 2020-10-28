b9e37ce1010fd33477a9f39bd7d228e457ba9e7c
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-10-28 16:54:49 +0900

    Use dynamic import

--- www/App.tsx
+++ www/App.tsx
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { loadNazuki } from './nazuki';
 
 export default function App () {
   const [result, setResult] = useState('');
+  const [errors, setErrors] = useState<any[]>([]);
+
+  const addError = (error: any) => {
+    setErrors((errors) => [...errors, error]);
+  };
 
   useEffect(() => {
     (async () => {
+      const { loadNazuki } = await import('./nazuki');
       const nazuki = await loadNazuki();
       setResult(await nazuki.generate(10));
-    })();
+    })().catch((err) => {
+      addError(`${err}`);
+    });
   }, []);
 
   return (
     <div>
       <h1>Nazuki</h1>
+      <div>
+        {errors.map((error) => (
+          <p>{error}</p>
+        ))}
+      </div>
       <textarea
         readOnly
         style={{ boxSizing: 'border-box', width: '100%', height: '10rem' }}
