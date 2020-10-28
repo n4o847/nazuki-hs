f3743c302ef6d315de7005d6a26d98be35b2d5f2
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-10-28 19:15:29 +0900

    Minimize JavaScript

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
@@ -26,6 +27,7 @@ module.exports = {
   },
   optimization: {
     minimizer: [
+      new TerserPlugin(),
       new CssMinimizerPlugin(),
     ],
   },
