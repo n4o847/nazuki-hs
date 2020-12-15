9f67d89d7c36d9775c2379be7bc10e9506cf6545
n4o847 <22975590+n4o847@users.noreply.github.com>
2020-12-15 15:57:15 +0900

    Format

--- app/Main.hs
+++ app/Main.hs
@@ -3,33 +3,32 @@
 
 module Main where
 
-import           System.Environment (getArgs)
-import           System.IO (stderr, hPutStrLn)
-import           Nazuki.Generator
-import qualified Nazuki.Intermediate.InstructionSet
-                                               as I
-import           Nazuki.Runner
+import qualified Nazuki.Intermediate.InstructionSet as I
+import Nazuki.Runner
+import System.Environment (getArgs)
+import System.IO (hPutStrLn, stderr)
 
 main :: IO ()
 main = do
-    args <- getArgs
-    case args of
-        ["gen"] ->
-            putStrLn program
-        ["len"] ->
-            print (length program)
-        ["run"] -> do
-            input <- getContents
-            either (hPutStrLn stderr) putStrLn $ run program input
-        ["debug"] -> do
-            input <- getContents
-            either (hPutStrLn stderr) putStrLn $ debug program input
-        _ ->
-            hPutStrLn stderr "Wrong arguments"
-    where
-        program = I.generate
-            [ I.Scan
-            , I.Scan
-            , I.Add
-            , I.Print
-            ]
+  args <- getArgs
+  case args of
+    ["gen"] ->
+      putStrLn program
+    ["len"] ->
+      print (length program)
+    ["run"] -> do
+      input <- getContents
+      either (hPutStrLn stderr) putStrLn $ run program input
+    ["debug"] -> do
+      input <- getContents
+      either (hPutStrLn stderr) putStrLn $ debug program input
+    _ ->
+      hPutStrLn stderr "Wrong arguments"
+  where
+    program =
+      I.generate
+        [ I.Scan,
+          I.Scan,
+          I.Add,
+          I.Print
+        ]
