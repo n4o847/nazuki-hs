1a919249283387b9b15a9652c2fb1888cd03aea9
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-02-24 22:11:15 +0900

    Add CLI commands

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,8 @@ module Main where
 import Data.Text as T
 import Data.Text.IO as TIO
 import qualified Nazuki.Assembler.Instruction as I
+import qualified Nazuki.Assembler.Label as L
+import qualified Nazuki.Assembler.Parser as P
 import Nazuki.Runner
 import System.Environment (getArgs)
 import System.IO (stderr)
@@ -18,7 +20,15 @@ main = do
       TIO.putStrLn program
     ["len"] ->
       print (T.length program)
+    ["asm", inFile, outFile] -> do
+      program <- TIO.readFile inFile
+      let result = I.generate <$> (L.resolveLabels =<< P.parse program)
+      either (TIO.hPutStrLn stderr) (TIO.writeFile outFile) result
     ["run"] -> do
+      input <- TIO.getContents
+      either (TIO.hPutStrLn stderr) TIO.putStrLn $ run program input
+    ["run", inFile] -> do
+      program <- TIO.readFile inFile
       input <- TIO.getContents
       either (TIO.hPutStrLn stderr) TIO.putStrLn $ run program input
     ["debug"] -> do
