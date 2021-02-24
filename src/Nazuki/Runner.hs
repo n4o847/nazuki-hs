507364aa35baea5809ede0e8bf8e8dfb0c702d10
n4o847 <22975590+n4o847@users.noreply.github.com>
2021-02-25 03:59:23 +0900

    Rewrite Runner

--- src/Nazuki/Runner.hs
+++ src/Nazuki/Runner.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BlockArguments #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Nazuki.Runner
@@ -10,63 +11,31 @@ import Control.Monad.Except
 import Control.Monad.State
 import qualified Data.ByteString as BS
 import Data.Functor ((<&>))
+import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Builder as TLB
 import Data.Text.Lazy.Builder.Int (decimal)
 import Data.Word (Word8)
+import Nazuki.Runtime.Parser
 import Text.Printf
 
-data Cmd
-  = Inc
-  | Dec
-  | Fwd
-  | Bwd
-  | Get
-  | Put
-  | While [Cmd]
-  deriving (Show)
+data Tape a = Tape [a] a [a]
 
-parse :: T.Text -> Either T.Text [Cmd]
-parse code = do
-  (rest, cmds) <- parse' code
-  case rest of
-    Nothing -> Right cmds
-    _ -> Left "unmatched ]"
-  where
-    parse' code =
-      case T.uncons code of
-        Nothing -> Right (Nothing, [])
-        Just ('+', xs) -> Inc <:> parse' xs
-        Just ('-', xs) -> Dec <:> parse' xs
-        Just ('>', xs) -> Fwd <:> parse' xs
-        Just ('<', xs) -> Bwd <:> parse' xs
-        Just (',', xs) -> Get <:> parse' xs
-        Just ('.', xs) -> Put <:> parse' xs
-        Just ('[', xs) -> do
-          (rest, block) <- parse' xs
-          case rest of
-            Just xs -> While block <:> parse' xs
-            Nothing -> Left "unmatched ["
-        Just (']', xs) -> Right (Just xs, [])
-        Just (_, xs) -> parse' xs
-    (<:>) = fmap . fmap . (:)
-
-data Tape a = Tape [a] a [a] deriving (Show)
-
-data BFState = BFState
+data RunnerState = RunnerState
   { mem :: Tape Word8,
     ptr :: Int,
     cnt :: Int,
     input :: [Word8],
     output :: [Word8]
   }
-  deriving (Show)
 
-emptyBFState :: BFState
-emptyBFState =
-  BFState
+type Runner = StateT RunnerState (Either Text)
+
+emptyRunnerState :: RunnerState
+emptyRunnerState =
+  RunnerState
     { mem = Tape [] 0 [],
       ptr = 0,
       cnt = 0,
@@ -74,87 +43,87 @@ emptyBFState =
       output = []
     }
 
-exec :: [Cmd] -> [Word8] -> Either T.Text BFState
+exec :: [Cmd] -> [Word8] -> Either Text RunnerState
 exec cmds inputData =
-  execStateT (exec' cmds) emptyBFState {input = inputData}
-  where
-    exec' :: [Cmd] -> StateT BFState (Either T.Text) ()
-    exec' cmds =
-      case cmds of
-        [] -> return ()
-        Inc : cs -> do
-          incCnt
-          updateMem increment
-          exec' cs
-        Dec : cs -> do
-          incCnt
-          updateMem decrement
-          exec' cs
-        Fwd : cs -> do
-          incCnt
-          updatePtr succ
-          updateMem forward
-          exec' cs
-        Bwd : cs -> do
-          incCnt
-          updatePtr pred
-          updateMem backward
-          exec' cs
-        Get : cs -> do
-          incCnt
-          state <- get
-          case input state of
-            x : xs -> do
-              put state {input = xs}
-              updateMem (\(Tape ls _ rs) -> Tape ls x rs)
-            [] ->
-              updateMem (\(Tape ls _ rs) -> Tape ls 255 rs)
-          exec' cs
-        Put : cs -> do
-          incCnt
-          state <- get
-          let Tape _ x _ = mem state
-          put state {output = x : output state}
-          exec' cs
-        While a : cs -> do
-          incCnt
-          state <- get
-          let Tape _ x _ = mem state
-          if x == 0
-            then exec' cs
-            else do
-              exec' a
-              exec' cmds
-    updateMem :: (Tape Word8 -> Tape Word8) -> StateT BFState (Either T.Text) ()
-    updateMem f = modify $ \state -> state {mem = f $ mem state}
-    increment (Tape ls x rs) = Tape ls (x + 1) rs
-    decrement (Tape ls x rs) = Tape ls (x - 1) rs
-    forward (Tape ls x []) = Tape (x : ls) 0 []
-    forward (Tape ls x (r : rs)) = Tape (x : ls) r rs
-    backward (Tape [] x rs) = Tape [] 0 (x : rs)
-    backward (Tape (l : ls) x rs) = Tape ls l (x : rs)
-    incCnt :: StateT BFState (Either T.Text) ()
-    incCnt = do
-      state <- get
-      let newCnt = cnt state + 1
-      if newCnt > 10000000
-        then throwError "operation limit exceeded"
-        else put state {cnt = newCnt}
-    updatePtr :: (Int -> Int) -> StateT BFState (Either T.Text) ()
-    updatePtr f = do
-      state <- get
-      let newPtr = f $ ptr state
-      if newPtr > 10000
-        then throwError "memory access error (> 10000)"
-        else
-          if newPtr < 0
-            then throwError "memory access error (< 0)"
-            else put state {ptr = newPtr}
+  execStateT (exec' cmds) emptyRunnerState {input = inputData}
 
-hex :: Word8 -> T.Text
+exec' :: [Cmd] -> Runner ()
+exec' cmds =
+  case cmds of
+    [] -> return ()
+    Plus x : cs -> do
+      incCnt
+      updateMem (plus x)
+      exec' cs
+    Step x : cs -> do
+      incCnt
+      updateMem (step x)
+      exec' cs
+    Get : cs -> do
+      incCnt
+      state <- get
+      case input state of
+        x : xs -> do
+          put state {input = xs}
+          updateMem (\(Tape ls _ rs) -> Tape ls x rs)
+        [] ->
+          updateMem (\(Tape ls _ rs) -> Tape ls 255 rs)
+      exec' cs
+    Put : cs -> do
+      incCnt
+      state <- get
+      let Tape _ x _ = mem state
+      put state {output = x : output state}
+      exec' cs
+    While a : cs -> do
+      incCnt
+      state <- get
+      let Tape _ x _ = mem state
+      if x == 0
+        then exec' cs
+        else do
+          exec' a
+          exec' cmds
+
+updateMem :: (Tape Word8 -> Tape Word8) -> Runner ()
+updateMem f =
+  modify $ \state -> state {mem = f $ mem state}
+
+plus :: Word8 -> Tape Word8 -> Tape Word8
+plus x (Tape ls a rs) = Tape ls (a + x) rs
+
+step :: Int -> Tape Word8 -> Tape Word8
+step x tape
+  | x == 0 = tape
+  | x > 0 = step (x - 1) case tape of
+    Tape ls a [] -> Tape (a : ls) 0 []
+    Tape ls a (r : rs) -> Tape (a : ls) r rs
+  | x < 0 = step (x + 1) case tape of
+    Tape [] a rs -> Tape [] 0 (a : rs)
+    Tape (l : ls) a rs -> Tape ls l (a : rs)
+
+incCnt :: Runner ()
+incCnt = do
+  state <- get
+  let newCnt = cnt state + 1
+  when (newCnt > 10000000) do
+    throwError "operation limit exceeded"
+  put state {cnt = newCnt}
+
+updatePtr :: (Int -> Int) -> Runner ()
+updatePtr f = do
+  state <- get
+  let newPtr = f $ ptr state
+  when (newPtr > 10000) do
+    throwError "memory access error (> 10000)"
+  when (newPtr < 0) do
+    throwError "memory access error (< 0)"
+  put state {ptr = newPtr}
+
+hex :: Word8 -> Text
 hex = T.pack . printf "%02X"
 
-inspect :: BFState -> Either T.Text T.Text
+inspect :: RunnerState -> Either Text Text
 inspect state =
   let Tape ls x rs = mem state
       out = reverse $ output state
@@ -182,16 +151,16 @@ inspect state =
                 <> "\n"
             )
 
-debug :: T.Text -> T.Text -> Either T.Text T.Text
+debug :: Text -> Text -> Either Text Text
 debug program input = parse program >>= flip exec (encode input) >>= inspect
 
-run :: T.Text -> T.Text -> Either T.Text T.Text
+run :: Text -> Text -> Either Text Text
 run program input = parse program >>= flip exec (encode input) <&> decode . reverse . output
 
-encode :: T.Text -> [Word8]
+encode :: Text -> [Word8]
 encode =
   BS.unpack . TE.encodeUtf8
 
-decode :: [Word8] -> T.Text
+decode :: [Word8] -> Text
 decode =
   TE.decodeUtf8 . BS.pack
---
+++ src/Nazuki/Runtime/Parser.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Nazuki.Runtime.Parser
+  ( Cmd (..),
+    parse,
+  )
+where
+
+import Control.Monad.Except
+import Control.Monad.State
+import Data.Functor ((<&>))
+import Data.Text (Text)
+import qualified Data.Text as Text
+import Data.Word (Word8)
+
+data Cmd
+  = Inc
+  | Dec
+  | Fwd
+  | Bwd
+  | Plus Word8
+  | Step Int
+  | Get
+  | Put
+  | While [Cmd]
+
+data ParserState = ParserState {rest :: Text, depth :: Int}
+
+type Parser = StateT ParserState (Either Text)
+
+emptyParserState :: ParserState
+emptyParserState =
+  ParserState {rest = "", depth = 0}
+
+getDepth :: Parser Int
+getDepth =
+  gets (\ParserState {depth} -> depth)
+
+setDepth :: Int -> Parser ()
+setDepth depth =
+  modify (\s -> s {depth})
+
+readChar :: Parser (Maybe Char)
+readChar = do
+  ParserState {rest} <- get
+  case Text.uncons rest of
+    Just (x, xs) -> do
+      modify (\s -> s {rest = xs})
+      return $ Just x
+    Nothing ->
+      return Nothing
+
+readCmds :: Parser [Cmd]
+readCmds = do
+  readChar >>= \case
+    Nothing -> do
+      depth <- getDepth
+      when (depth /= 0) do
+        throwError "unmatched ["
+      return []
+    Just '+' ->
+      readCmds <&> \case
+        Plus 255 : xs -> xs
+        Plus x : xs -> Plus (x + 1) : xs
+        xs -> Plus 1 : xs
+    Just '-' ->
+      readCmds <&> \case
+        Plus 1 : xs -> xs
+        Plus y : xs -> Plus (y - 1) : xs
+        xs -> Plus 255 : xs
+    Just '>' ->
+      readCmds <&> \case
+        Step (-1) : xs -> xs
+        Step x : xs -> Step (x + 1) : xs
+        xs -> Step 1 : xs
+    Just '<' ->
+      readCmds <&> \case
+        Step 1 : xs -> xs
+        Step x : xs -> Step (x - 1) : xs
+        xs -> Step (-1) : xs
+    Just ',' ->
+      (Get :) <$> readCmds
+    Just '.' -> do
+      (Put :) <$> readCmds
+    Just '[' -> do
+      depth <- getDepth
+      setDepth (depth + 1)
+      block <- readCmds
+      (While block :) <$> readCmds
+    Just ']' -> do
+      depth <- getDepth
+      when (depth == 0) do
+        throwError "unmatched ]"
+      setDepth (depth - 1)
+      return []
+    Just _ ->
+      readCmds
+
+parse :: Text -> Either Text [Cmd]
+parse code =
+  evalStateT readCmds emptyParserState {rest = code}
