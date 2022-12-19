0e30dcb26f49ae2b5a13db53f8b81fa6c3e70810
n4o847 <22975590+n4o847@users.noreply.github.com>
2022-12-20 01:00:34 +0900

    chore(site): update react and typescript

--- site/main.tsx
+++ site/main.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
