5444a0af5d6a6cd869014bc0f4774c9246193905
n4o847 <22975590+n4o847@users.noreply.github.com>
2022-12-20 00:50:26 +0900

    chore(site): migrate to Vite

--- site/main.tsx
+++ site/main.tsx
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('root'));
