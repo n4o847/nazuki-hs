e7477c651e88bf3af033e72c354821f278af2fe4
n4o847 <22975590+n4o847@users.noreply.github.com>
2023-07-09 18:43:32 +0900

    chore(website): move introduction page

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -14,7 +14,10 @@ function Header() {
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link className="button button--secondary button--lg" to="/docs">
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/introduction"
+          >
             Docs
           </Link>
         </div>
