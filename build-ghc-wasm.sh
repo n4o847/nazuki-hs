c75853297c31d9bfd1efe894a148cba8151d7a01
n4o847 <22975590+n4o847@users.noreply.github.com>
2024-06-22 20:07:20 +0900

    chore: update to GHC 9.8

--- build-ghc-wasm.sh
+++ build-ghc-wasm.sh
@@ -2,8 +2,8 @@
 
 case $1 in
   --docker)
-    docker build -t ghc-wasm-meta:9.6 --network host ghc-wasm-meta
-    docker run --rm -v $(pwd):/workspace -w /workspace --net host ghc-wasm-meta:9.6 ./entrypoint-ghc-wasm.sh
+    docker build -t ghc-wasm-meta:9.8 --network host ghc-wasm-meta
+    docker run --rm -v $(pwd):/workspace -w /workspace --net host ghc-wasm-meta:9.8 ./entrypoint-ghc-wasm.sh
     ;;
   --local)
     source ~/.ghc-wasm/env
